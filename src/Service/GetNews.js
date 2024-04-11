d0f248f5dea422e01a8d770b5ebb9a5066cb9080
kinjalsinghh <118274996+kinjalsinghh@users.noreply.github.com>
2024-04-11 22:03:27 +0530

    Update GetNews.js

--- src/Service/GetNews.js
+++ src/Service/GetNews.js
@@ -3,7 +3,9 @@ function GetNews(category){
     const API_Key='2141e5cb37d54ff0bf1df69860ceb6eb';
     const API_ENDPOINT=`https://newsapi.org/v2/top-headlines?country=in&category=${category}`
 
-       return axios.get(`${API_ENDPOINT}&apiKey=${API_Key}`)
+       return axios.get(`${API_ENDPOINT}&apiKey=${API_Key}`).catch((err)=>{
+      <h1>Error</h1>
+    })
 
 }
 export default GetNews;
